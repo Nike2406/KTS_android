03eaada62e4a48ae8479f3a9bbb5c2d76b43a021
Nike2406 <locus2@mail.ru>
2021-09-18 14:52:06 +0300

    add button, picture and test app

--- app/src/main/java/com/example/kts_android/MainActivity.kt
+++ app/src/main/java/com/example/kts_android/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.kts_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var textView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.text)
+        findViewById<Button>(R.id.button).setOnClickListener{
+            val newText = "Bye world!"
+            textView?.text = newText
+        }
     }
 }
